eec121607f380456818b398a31d9688752f1c3fa
Igor Jelen <igorjelen@gmail.com>
2020-11-04 10:58:31 +0000

    Use .env for Firebase config

--- src/App.js
+++ src/App.js
@@ -5,27 +5,20 @@ import VehicleCreate from './components/VehicleCreate';
 import VehicleEdit from './components/VehicleEdit';
 import CommuteTwoToneIcon from '@material-ui/icons/CommuteTwoTone';
 import firebase from 'firebase';
-// import Dashboard from './components/Dashboard'
-
-// https://github.com/stevekinney/think-piece
-
-// import CustomLoginPage from './components/CustomLoginPage';
-
 import {
   FirebaseAuthProvider,
-  FirebaseDataProvider,
-  // FirebaseRealTimeSaga
+  FirebaseDataProvider
 } from 'react-admin-firebase';
 
  const firebaseConfig = {
-    apiKey: "AIzaSyDFOxWWXOfBV1K6sMY1YI4O5uLtHQLpuuQ",
-    authDomain: "evidencija-registracija.firebaseapp.com",
-    databaseURL: "https://evidencija-registracija.firebaseio.com",
-    projectId: "evidencija-registracija",
-    storageBucket: "evidencija-registracija.appspot.com",
-    messagingSenderId: "217728791466",
-    appId: "1:217728791466:web:84998272648ae5b2e931d3",
-    measurementId: "G-1RMEMNBEJ9"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    databaseURL: process.env.REACT_APP_databaseURL,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId
   };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
