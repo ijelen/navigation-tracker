7fbd6f33f1b23b44f395b062deae61b6e2548b05
Igor Jelen <igorjelen@gmail.com>
2020-11-06 14:18:39 +0000

    Use custom Mui List for small screen

--- src/components/VehicleList.js
+++ src/components/VehicleList.js
@@ -1,6 +1,11 @@
 import React from 'react'
-import { List, SimpleList, Datagrid, TextField, DateField, EditButton, DeleteButton } from 'react-admin';
+import { List, useListContext, Datagrid, TextField, DateField, EditButton, DeleteButton } from 'react-admin';
 import { useMediaQuery } from '@material-ui/core';
+
+import {List as MuiList} from '@material-ui/core';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Grid from '@material-ui/core/Grid';
 
 const VehicleList = (props) => {
 
@@ -19,28 +24,6 @@ const VehicleList = (props) => {
         }
     };
 
-    const myPrimaryText = (record) => {
-        return (
-            <span key={record.name}>{capitalizeFirstLetter(record.type)} {record.name}</span>
-        );
-    };
-
-    const mySecondaryText = (record) => {
-        return (
-            <div>
-                <span>{record.registration}, {record.chassis} {record.code && `(${record.code})`}</span>
-                <span>vlasnik: {record.owner}</span>
-            </div>
-        );
-    }
-
-    const myTertiaryText = (record) => {
-        const date = new Date(Date.parse(record.expires));
-        return (
-            <span className="yuyu" style={{ color: formatRecord(record, {expired: "#e6101b", expiring: "rgb(196, 180, 7)"}) }}>{date.toLocaleDateString("hr", { weekday: 'short', year: 'numeric', month: 'long', day: 'numeric' })}</span>
-            )
-    };
-
     const postRowStyle = (record, index) => {
         return ({
             backgroundColor: formatRecord(record, {expired: "rgba(252, 121, 132, 0.75)", expiring: "rgba(247, 232, 96, 0.83)"})
@@ -51,19 +34,50 @@ const VehicleList = (props) => {
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
 
+    const MySimpleList = (props) => {
+        const { ids, data, basePath } = useListContext();
+        return (
+            <MuiList>
+                {ids.map(id =>
+                <ListItem 
+                    style={{backgroundColor: formatRecord(data[id], {expired: "rgba(252, 121, 132, 0.75)", expiring: "rgba(247, 232, 96, 0.83)"})}} 
+                    key={id} 
+                    button 
+                    component="a" 
+                    href={`#${basePath}/${id}`}
+                    >
+                    <ListItemText 
+                        primary={(
+                            <Grid container justify="space-between">
+                                <Grid item>
+                                   {capitalizeFirstLetter(data[id].type)} {data[id].name}
+                                </Grid>
+                                <Grid item>
+                                    <DateField record={data[id]} source="expires" locales="hr" options={{ weekday: 'short', year: 'numeric', month: 'long', day: 'numeric' }} />
+                                </Grid>
+                            </Grid>
+                            )}
+                        secondary={(
+                            <div>
+                                <span>{data[id].registration}, {data[id].chassis} {data[id].code && `(${data[id].code})`}</span>
+                                <span>vlasnik: {data[id].owner}</span>
+                            </div>
+                            )} 
+                    />
+                </ListItem>
+            )}
+        </MuiList>
+        )
+    }
+
     const isSmall = useMediaQuery(theme => theme.breakpoints.down('sm'));
 
     return (
         <List title="Evidencija registracija" bulkActionButtons={false} {...props}>
             {isSmall ? (
-                <SimpleList
-                    primaryText={record => myPrimaryText(record)}
-                    secondaryText={record => mySecondaryText(record)}
-                    tertiaryText={record => myTertiaryText(record)}
-                    linkType="edit"
-                />
+                 <MySimpleList/>
             ) : (
-                    <Datagrid rowStyle={postRowStyle}>
+                <Datagrid rowStyle={postRowStyle}>
                         <TextField label="Stroj " source='name' />
                         <TextField label="Tip stroja" source="type" />
                         <TextField label="Reg. oznaka" source='registration' />
